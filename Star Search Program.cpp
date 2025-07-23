3ad3cd55cdc682580e4fbef0554cea4c82ba8e9a
jm underwood <jm.underwood@hotmail.com>
2025-07-23 17:10:54 -0400

    Added Comments

--- Star Search Program.cpp
+++ Star Search Program.cpp
@@ -18,17 +18,17 @@ double getScore(double &score)
 	return score;
 }
 
-bool isLower(double lower, double higher)
+bool isLower(double lower, double higher) // If the first value is lower than the second value, return true
 {
     return lower <= higher;
 }
 
-bool isHigher(double higher, double lower)
+bool isHigher(double higher, double lower) // If the first value is higher than the second value, return true.
 {
     return higher >= lower;
 }
 
-double calcAverage(double max, double min, double tot)
+double calcAverage(double max, double min, double tot) // Returns the value of the average of the 3 middle values
 {
 	return (tot - (max + min)) / 3;
 }
@@ -36,7 +36,7 @@ double calcAverage(double max, double min, double tot)
 int main()
 {
 	double minScore, maxScore, total;
-	double score[5];
+	double score[5]; // This is an array. It can fit up to 5 values under one variable name, making the code simpler
 	double average;
 
     getScore(score[0]);
@@ -46,17 +46,17 @@ int main()
 
 	for (int x = 1; x <=4 ; x++)
 	{
-		getScore(score[x]);
-		total += score[x];
+		getScore(score[x]); // Gets the score of the first value input into the array
+		total += score[x]; // Adds the value of of the previous score to the next score that was input
 
 		if (isLower(score[x], minScore))
 		{
-			minScore = score[x];
+			minScore = score[x]; // Sets the value of min score to an input value if said value is lower than the current min score value
 		}
 
 		if (isHigher(score[x], maxScore))
 		{
-			maxScore = score[x];
+			maxScore = score[x];  // Sets the value of max score to an input value if said value is higher than the current max score value
 		}
 	}
 
